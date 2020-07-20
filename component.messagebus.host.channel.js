56d1d0a687ddf921ea77d344a2741e91f18aef82
marchuanv <marchuanv@gmail.com>
2020-07-20 13:50:56 +0200

    refactor

--- component.messagebus.host.channel.js
+++ component.messagebus.host.channel.js
@@ -6,17 +6,44 @@ const logging = require("logging");
 logging.config.add("MessageBus Channel");
 const thisModule = "component.messagebus.host.channel";
 module.exports = { 
-    handle: (callingModule, options) => {
+    handle: ({ callingSubscriberModule, callingPublisherModule }, options) => {
         delegate.register(thisModule, ({ host }) => {
-            const channelModule = `component.messagebus.host.channel.register.${host.publicHost}.${host.publicPort}`;
-            delegate.register(channelModule, async ({ data }) => {
+            const thisModuleRegisterChannel = `component.messagebus.host.channel.register.${host.publicHost}.${host.publicPort}`;
+            delegate.register(thisModuleRegisterChannel, async ({ data }) => {
+                
                 const {channel} = utils.getJSONObject(data) || {};
                 if ( !channel){
                     return { headers: { "Content-Type":"text/plain"}, statusCode: 400, statusMessage: "Bad Request", data: "channel is required" };
                 }
-                await delegate.call(callingModule, { channel });
+                
+                const thisModuleChannelPublish = `component.messagebus.${host.publicHost}${channel}.publish`;
+                delegate.register(thisModuleChannelPublish, async ({ data }) => {
+                    return await delegate.call(callingPublisherModule, { data });
+                });
+                requestHandlerSecure.handle(thisModuleChannelPublish, {
+                    publicHost: host.publicHost,
+                    publicPort: host.publicPort,
+                    privateHost: host.privateHost,
+                    privatePort: host.privatePort,
+                    path: `/${channel}/publish`,
+                    hashedPassphrase: host.hashedPassphrase,
+                    hashedPassphraseSalt: host.hashedPassphraseSalt
+                });
+                const thisModuleChannelSubscribe = `component.messagebus.${host.publicHost}${channel}.subscribe`;
+                delegate.register(thisModuleChannelSubscribe, async ({ data }) => {
+                    return await delegate.call(callingSubscriberModule, { data });
+                });
+                requestHandlerSecure.handle(thisModuleChannelSubscribe, {
+                    publicHost: host.publicHost,
+                    publicPort: host.publicPort,
+                    privateHost: host.privateHost,
+                    privatePort: host.privatePort,
+                    path: `/${channel}/subscribe`,
+                    hashedPassphrase: host.hashedPassphrase,
+                    hashedPassphraseSalt: host.hashedPassphraseSalt
+                });
             });
-            requestHandlerSecure.handle(channelModule, {
+            requestHandlerSecure.handle(thisModuleRegisterChannel, {
                 publicHost: host.publicHost,
                 publicPort: host.publicPort,
                 privateHost: host.privateHost,
--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 const messagebusChannel = require("./component.messagebus.host.channel.js");
 const delegate = require("component.delegate");
 (async()=>{ 
-    const callingModule = "component.messagebus.publisher";
-    delegate.register(callingModule, () => {
-        return { statusCode: 200, statusMessage: "Success", headers: {}, data: null };
+    const callingPublisherModule = "component.messagebus.publisher";
+    const callingSubscriberModule = "component.messagebus.subscriber";
+    delegate.register(callingPublisherModule, () => {
+        return { statusCode: 200, statusMessage: "Success", headers: {}, data: "published" };
     });
-    await messagebusChannel.handle({ publicHost: "localhost", publicPort: 3000, privateHost: "localhost", privatePort: 3000 });
+    delegate.register(callingSubscriberModule, () => {
+        return { statusCode: 200, statusMessage: "Success", headers: {}, data: "subscribed" };
+    });
+    await messagebusChannel.handle({ callingPublisherModule, callingSubscriberModule }, { publicHost: "localhost", publicPort: 3000, privateHost: "localhost", privatePort: 3000 });
 })().catch((err)=>{
     console.error(err);
 });
