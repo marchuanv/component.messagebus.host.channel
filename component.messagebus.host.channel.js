962ecf23bc4283d5c9b3f50aa550f0f1dceedfaa
marchuanv <marchuanv@gmail.com>
2020-07-20 11:56:33 +0200

    fixed

--- component.messagebus.host.channel.js
+++ component.messagebus.host.channel.js
@@ -6,7 +6,7 @@ const logging = require("logging");
 logging.config.add("MessageBus Channel");
 const thisModule = "component.messagebus.host.channel";
 module.exports = { 
-    handle: ( options) => {
+    handle: (callingModule, options) => {
         delegate.register(thisModule, ({ host }) => {
             const channelModule = `component.messagebus.host.channel.register.${host.publicHost}.${host.publicPort}`;
             delegate.register(channelModule, async ({ data }) => {
@@ -14,7 +14,6 @@ module.exports = {
                 if ( !channel){
                     return { headers: { "Content-Type":"text/plain"}, statusCode: 400, statusMessage: "Bad Request", data: "channel is required" };
                 }
-                const callingModule = `component.messagebus.publisher.${channel}`;
                 return await delegate.call(callingModule, { channel });
             });
             requestHandlerSecure.handle(channelModule, {
