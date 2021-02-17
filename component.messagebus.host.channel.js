58eb28d0b3d04161d7aee6eb731cc101d8536b22
marchuanv <marchuanv@gmail.com>
2021-02-18 01:49:17 +0200

    refactor

--- component.messagebus.host.channel.js
+++ component.messagebus.host.channel.js
@@ -20,7 +20,8 @@ module.exports = {
                 const publisherChannelContextName = `${host.port}/${channel}/publish`;
                 const channelPublishContext = `component.messagebus.publish`;
                 delegate.register(channelPublishContext, publisherChannelContextName, async ({ headers: { username, fromhost, fromport },data }) => {
-                    return await delegate.call({ context: `component.messagebus.publisher` }, { channel: { host: host.name, port: host.port, name: channel }, username, fromhost, fromport, data });
+                    await delegate.call({ context: `component.messagebus.publisher` }, { channel: { host: host.name, port: host.port, name: channel }, username, fromhost, fromport, data });
+                    return { statusCode: 200, statusMessage: "Publish Successful", headers: {}, data };
                 });
                 requestHandlerSecure.handle(channelPublishContext, {
                     host: host.name,
@@ -32,8 +33,9 @@ module.exports = {
 
                 const subscriberChannelContextName = `${host.port}/${channel}/subscribe`;
                 const channelSubscribeContext = `component.messagebus.subscribe`;
-                delegate.register(channelSubscribeContext, subscriberChannelContextName, async ({ headers: { username, fromhost,fromport }, data }) => {
-                    return await delegate.call({ context: `component.messagebus.subscriber` }, { channel: { host: host.name, port: host.port, name: channel }, username, fromhost, fromport, data });
+                delegate.register(channelSubscribeContext, subscriberChannelContextName, async ({ headers: { username, fromhost, fromport }, data }) => {
+                    await delegate.call({ context: `component.messagebus.subscriber` }, { channel: { host: host.name, port: host.port, name: channel }, username, fromhost, fromport, data });
+                    return { statusCode: 200, statusMessage: "Subscribe Successful", headers: {}, data };
                 });
                 requestHandlerSecure.handle(channelSubscribeContext, {
                     host: host.name,
