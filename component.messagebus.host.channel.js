e3c8b5e6435b2050110ffc1c52816aa81bfb4ee2
marchuanv <marchuanv@gmail.com>
2021-02-18 00:13:55 +0200

    refactor

--- component.messagebus.host.channel.js
+++ component.messagebus.host.channel.js
@@ -19,8 +19,8 @@ module.exports = {
 
                 const publisherChannelContextName = `${host.port}/${channel}/publish`;
                 const channelPublishContext = `component.messagebus.publish`;
-                delegate.register(channelPublishContext, publisherChannelContextName, async ({ headers: { fromhost, fromport },data }) => {
-                    return await delegate.call({ context: `component.messagebus.publisher` }, { fromhost, fromport, channel, data });
+                delegate.register(channelPublishContext, publisherChannelContextName, async ({ headers: { username, fromhost, fromport },data }) => {
+                    return await delegate.call({ context: `component.messagebus.publisher` }, { username, fromhost, fromport, channel, data });
                 });
                 requestHandlerSecure.handle(channelPublishContext, {
                     host: host.name,
@@ -32,8 +32,8 @@ module.exports = {
 
                 const subscriberChannelContextName = `${host.port}/${channel}/subscribe`;
                 const channelSubscribeContext = `component.messagebus.subscribe`;
-                delegate.register(channelSubscribeContext, subscriberChannelContextName, async ({ headers: { fromhost,fromport }, data }) => {
-                    return await delegate.call({ context: `component.messagebus.subscriber` }, { fromhost, fromport, channel, data });
+                delegate.register(channelSubscribeContext, subscriberChannelContextName, async ({ headers: { username, fromhost,fromport }, data }) => {
+                    return await delegate.call({ context: `component.messagebus.subscriber` }, { username, fromhost, fromport, channel, data });
                 });
                 requestHandlerSecure.handle(channelSubscribeContext, {
                     host: host.name,
