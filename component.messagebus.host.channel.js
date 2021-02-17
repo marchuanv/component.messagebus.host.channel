8035dd7faa5dd1de491b2b74120b875a8eb72cf6
marchuanv <marchuanv@gmail.com>
2021-02-17 16:17:46 +0200

    refactor

--- component.messagebus.host.channel.js
+++ component.messagebus.host.channel.js
@@ -6,41 +6,38 @@ const logging = require("logging");
 logging.config.add("MessageBus Channel");
 module.exports = { 
     handle: (options) => {
-        const name = `${options.publicPort}/channel`;
+        const name = `${options.port}/channel`;
         delegate.register("component.messagebus.host.channel", name, ({ host }) => {
             const registerChannelContext = `component.messagebus.host.channel.register`;
-            const registerContextName = `${host.publicPort}/channel/register`;
+            const registerContextName = `${host.port}/channel/register`;
             delegate.register(registerChannelContext, registerContextName, async ({  data }) => {
                 
                 const { channel } = utils.getJSONObject(data) || {};
                 if (!channel){
                     return { headers: { "Content-Type":"text/plain"}, statusCode: 400, statusMessage: "Bad Request", data: "channel is required" };
                 }
-                const channelContextName = `${host.publicPort}/channel/${channel}`;
-                
+
+                const publisherChannelContextName = `${host.port}/${channel}/publish`;
                 const channelPublishContext = `component.messagebus.publish`;
-                delegate.register(channelPublishContext, channelContextName, async ({ headers: { fromhost, fromport },data }) => {
+                delegate.register(channelPublishContext, publisherChannelContextName, async ({ headers: { fromhost, fromport },data }) => {
                     await delegate.call({ context: `component.messagebus.publisher` }, { fromhost, fromport, channel, data });
                 });
                 requestHandlerSecure.handle(channelPublishContext, {
-                    publicHost: host.publicHost,
-                    publicPort: host.publicPort,
-                    privateHost: host.privateHost,
-                    privatePort: host.privatePort,
+                    host: host.name,
+                    port: host.port,
                     path: `/${channel}/publish`,
                     hashedPassphrase: host.hashedPassphrase,
                     hashedPassphraseSalt: host.hashedPassphraseSalt
                 });
 
+                const subscriberChannelContextName = `${host.port}/${channel}/subscribe`;
                 const channelSubscribeContext = `component.messagebus.subscribe`;
-                delegate.register(channelSubscribeContext, channelContextName, async ({ headers: { fromhost,fromport }, data }) => {
+                delegate.register(channelSubscribeContext, subscriberChannelContextName, async ({ headers: { fromhost,fromport }, data }) => {
                     await delegate.call({ context: `component.messagebus.subscriber` }, { fromhost, fromport, channel, data });
                 });
                 requestHandlerSecure.handle(channelSubscribeContext, {
-                    publicHost: host.publicHost,
-                    publicPort: host.publicPort,
-                    privateHost: host.privateHost,
-                    privatePort: host.privatePort,
+                    host: host.name,
+                    port: host.port,
                     path: `/${channel}/subscribe`,
                     hashedPassphrase: host.hashedPassphrase,
                     hashedPassphraseSalt: host.hashedPassphraseSalt
@@ -48,10 +45,8 @@ module.exports = {
 
             });
             requestHandlerSecure.handle(registerChannelContext, {
-                publicHost: host.publicHost,
-                publicPort: host.publicPort,
-                privateHost: host.privateHost,
-                privatePort: host.privatePort,
+                host: host.name,
+                port: host.port,
                 path: "/channel/register",
                 hashedPassphrase: host.hashedPassphrase,
                 hashedPassphraseSalt: host.hashedPassphraseSalt
--- test.js
+++ test.js
@@ -1,16 +1,101 @@
 const messagebusChannel = require("./component.messagebus.host.channel.js");
 const delegate = require("component.delegate");
+const request = require("component.request");
 (async()=>{ 
+   
+    await messagebusChannel.handle({ host: "localhost", port: 3000 });
+    
+    //Register New Host
+    const newHost = { host: "localhost", port: "6000" };
+    let results = await request.send({
+        host: "localhost",
+        port: 3000,
+        path: "/host",
+        method: "GET",
+        headers: { 
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000,
+            passphrase: "secure1"
+        }, 
+        data: JSON.stringify(newHost),
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "New Request To Register New Host Test Failed";
+    }
+
+    
+    //New Request To Secured Host To Get Token
+    results = await request.send({
+        host: newHost.host,
+        port: newHost.port,
+        path: "/channel/register",
+        method: "GET",
+        headers: { 
+            username: "marchuanv",
+            fromhost: "bob",
+            fromport: 8000,
+            passphrase: "secure1"
+        }, 
+        data: "",
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "New Request To Secured Host To Get Token Test Failed";
+    }
+    const token = results.headers.token;
+
+    //New Request To Secured Host To Register A Channel
+    results = await request.send({
+        host: newHost.host,
+        port: newHost.port,
+        path: "/channel/register",
+        method: "GET",
+        headers: { 
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000,
+            token
+        }, 
+        data: `{ "channel":"apples" }`,
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "New Request To Secured Host To Register A Channel Test Failed";
+    }
+
     const publisherContext = "component.messagebus.publisher";
     const subscriberContext = "component.messagebus.subscriber";
-    const contextName = `3000.apples`;
+    const contextName = `${newHost.port}/apples`;
     delegate.register(publisherContext, contextName, () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: "published" };
     });
     delegate.register(subscriberContext, contextName, () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: "subscribed" };
     });
-    await messagebusChannel.handle({ publicHost: "localhost", publicPort: 3000, privateHost: "localhost", privatePort: 3000 });
+
+    //New Publish Request To Secured Host Channel
+    results = await request.send({
+        host: newHost.host,
+        port: newHost.port,
+        path: "/apples/publish",
+        method: "POST",
+        headers: { 
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000,
+            token
+        }, 
+        data: ``,
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "New Publish Request To Secured Host Channel";
+    }
+
+    process.exit();
+
 })().catch((err)=>{
     console.error(err);
 });
